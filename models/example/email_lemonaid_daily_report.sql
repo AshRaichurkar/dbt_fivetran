d695569d84a9ddbfa195933f53717761286b63ca
Ashutosh Raichurkar <109172140+AshRaichurkar@users.noreply.github.com>
2023-05-18 15:58:41 -0400

    Update email_lemonaid_daily_report.sql

--- models/example/email_lemonaid_daily_report.sql
+++ models/example/email_lemonaid_daily_report.sql
@@ -17,6 +17,7 @@ SELECT date
        , transactions
        , bounces
        , bounce_rate
+       , fnv_hash(date, fnv_hash(campaign,  fnv_hash(keyword))) as lemonaid_sk
         from hl.fivetran.email_lemonaid_daily_report_raw where to_date(cast(_modified as TEXT),'YYYY-MM-DD') >= '2023-01-01'
         and _file in (select _file from hl.fivetran.email_lemonaid_daily_report_raw where to_date(cast(_modified as TEXT),'YYYY-MM-DD') >= '2023-01-01')
         group by
